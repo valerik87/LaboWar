66a5f0eeae06c61bd75d609f4b2b52a6943f2072
DESKTOP-DQ6JIAP\Vuvvi <valerio.ceraudo@gmail.com>
2018-04-17 01:14:30 +0200

    City placeholder plus army placeholder homemade and simple army movement

---
+++ LaboWars/Assets/Scripts/ArmyMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmyMovement : MonoBehaviour {
+
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(Vector3.zero, Vector3.forward, Speed);
+    }
+}
