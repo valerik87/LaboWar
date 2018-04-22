bdb34457980f9c3cd6d7edbc7dbee3da8db8cd86
DESKTOP-DQ6JIAP\Vuvvi <valerio.ceraudo@gmail.com>
2018-04-23 00:29:33 +0200

    Did Parabola vertex calculus to define trajectory from army to target. Need to graphic the attack but can be usefull have a pool allocator for attack obj

--- LaboWars/Assets/Scripts/AttackAreaInfo.cs
+++ LaboWars/Assets/Scripts/AttackAreaInfo.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using Assets.Scripts;
 
 public class AttackAreaInfo : MonoBehaviour {
 
     public CircleCollider2D attackAreaCollider;
     public CircleRenderer circleRenderer;
 
-    private List<GameObject> targets; 
+    private float radius = 0;
+    private List<GameObject> targets;
+
+    //Not enough, i need to get an istance and use untill targe it's in area, then release. PoolAllocator should be a static singleton where to ask instance
+    //of AttackObj
+    private PoolAllocator<LineRenderer> lineRendererPool;
+    
     // Use this for initialization
     void Start () {
         targets = new List<GameObject>();
+        lineRendererPool = new PoolAllocator<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -20,7 +28,7 @@ public class AttackAreaInfo : MonoBehaviour {
         {
             foreach(GameObject target in targets)
             {
-                BezierCurve(target);
+                ParabolaTrajectoryTo(target);
             }            
         }        
     }
@@ -30,6 +38,7 @@ public class AttackAreaInfo : MonoBehaviour {
         circleRenderer.checkValuesChanged = true;
         circleRenderer.radius = NewRadius;
         attackAreaCollider.radius = NewRadius;
+        radius = NewRadius;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -41,13 +50,44 @@ public class AttackAreaInfo : MonoBehaviour {
         }
     }
 
-    void BezierCurve(GameObject other)
+    void OnTriggerExit2D(Collider2D other)
     {
-        // Circompherence x2 + y2 + ax + by +c = 0
-        Vector3 PositionZero = Vector3.zero - this.transform.position;
-        Vector3 ZeroTarget = other.transform.position - Vector3.zero;
+        if (targets.Contains(other.gameObject))
+        {
+            targets.Remove(other.gameObject);
+        }
+    }
 
-        Debug.DrawLine(this.transform.position, Vector3.zero, Color.red);
-        Debug.DrawLine(Vector3.zero, other.transform.position, Color.green);
+    void ParabolaTrajectoryTo(GameObject other)
+    {
+        float x = 0;
+        float cosx = 0;
+        float y = 0;
+        float angle = 0;
+        //Target coord in local view
+        Vector3 targetCoordInLocal = other.transform.InverseTransformPoint(this.transform.position);
+        cosx = (targetCoordInLocal.x/2) / radius;
+        angle = Mathf.Acos(cosx);
+        x = this.transform.localPosition.x + Mathf.Cos(Mathf.PI - angle) * radius;   
+        y = this.transform.localPosition.y + Mathf.Sin(Mathf.PI - angle) * radius;
+                                            
+        //Debug.Log("TargetCoordInLocal = " + targetCoordInLocal);
+        //Debug.Log("cosx = " + cosx);
+        //Debug.Log("angle = " + angle*Mathf.Rad2Deg);
+        //Debug.Log("x = " + x);
+        //Debug.Log("y = " + y);
+        
+        //Move in world coord
+        Vector3 ParabolaVertexInLocalWorld = this.transform.TransformPoint(new Vector3(x, y, this.transform.localPosition.z));
+        //Debug.Log("ParabolaVertexInLocalWorld " + ParabolaVertexInLocalWorld + " to obj " + other.name);
+
+        Vector3 PositionZero = ParabolaVertexInLocalWorld - this.transform.position;
+        Vector3 ZeroTarget = other.transform.position - ParabolaVertexInLocalWorld;
+
+        Debug.DrawLine(this.transform.position, ParabolaVertexInLocalWorld, Color.red);
+        Debug.DrawLine(ParabolaVertexInLocalWorld, other.transform.position, Color.green);
+
+        LineRenderer linerenderer = this.gameObject.GetComponent<LineRenderer>();
+        Assert.IsTrue(linerenderer == null);
     }
 }
---
+++ LaboWars/Assets/Scripts/PoolAllocator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public class PoolAllocator<T> where T : new()
+    {
+        private Stack<T> _items = new Stack<T>();
+        private object _sync = new object(); 
+          
+        public T Get()
+        {
+            lock (_sync)
+            {
+                if (_items.Count == 0)
+                {
+                    return new T();
+                }
+                else
+                {
+                    return _items.Pop();
+                }
+            }
+        }
+          
+        public void Free(T item)
+        {
+            lock (_sync)
+            {
+                _items.Push(item);
+            }
+        }
+    }
+}
